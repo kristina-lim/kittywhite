74d58ec43ce2667d91432f98b8c86165dfe9c7d4
kristina-lim <kristinahlim@gmail.com>
2023-01-10 15:11:47 -0800

    Define home view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Define the home view
+def home (request):
+  # Include a .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
