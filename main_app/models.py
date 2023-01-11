9fc2bcb035828ff5055e3533c7040f0e2802efa5
kristina-lim <kristinahlim@gmail.com>
2023-01-10 17:16:14 -0800

    Add characters model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-
+class Character(models.Model):
+  name = models.CharField(max_length=100)
+  gender = models.CharField(max_length=100)
+  birthday = models.CharField(max_length=100)
+  species = models.CharField(max_length=100)
+  description = models.TextField(max_length=350)
+  
