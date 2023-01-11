234fcb0984708843ab7c9ee7874ae9da48a814a1
kristina-lim <kristinahlim@gmail.com>
2023-01-11 14:52:36 -0800

    Add a custom method to check status

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -20,6 +21,9 @@ class Character(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'character_id': self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('feeding date')
