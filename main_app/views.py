4b493f8ec1f256eba3e7b6b3cafec017bb0f87b6
kristina-lim <kristinahlim@gmail.com>
2023-01-10 16:29:59 -0800

    Use python list containing a couple of character dictionaries

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+characters = [
+  {
+    'name': 'Hello Kitty',
+    'species': 'person',
+    'description': 'Born in the suburbs of London. She lives with her parents and her twin sister Mimmy who is her bestfriend.',
+    'birthday': 'November 1'
+  },
+  {
+    'name': 'Mimmy White',
+    'species': 'person',
+    'description': 'Born in the suburbs of London. She lives with her parents and her twin sister Kitty who is her bestfriend.',
+    'birthday': 'November 1'
+  },
+]
+
 # Define the home view
 def home(request):
   # Include a .html file extension - unlike when rendering EJS templates
