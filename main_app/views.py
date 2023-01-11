f744f0f8eaad97f36404afa6b4fd12b3b6fa0e9e
kristina-lim <kristinahlim@gmail.com>
2023-01-10 17:42:52 -0800

    Use the create method in the django model manager and update the character model

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,5 @@
 from django.shortcuts import render
-
-characters = [
-  {
-    'name': 'Hello Kitty',
-    'gender': 'female',
-    'birthday': 'November 1',
-    'species': 'Cat',
-    'description': 'Born in the suburbs of London. She lives with her parents and her twin sister Mimmy who is her bestfriend.',
-  },
-  {
-    'name': 'Mimmy White',
-    'gender': 'female',
-    'birthday': 'November 1',
-    'species': 'Cat',
-    'description': 'Born in the suburbs of London. She lives with her parents and her twin sister Kitty who is her bestfriend.',
-  },
-]
+from .models import Character
 
 # Define the home view
 def home(request):
@@ -26,6 +10,7 @@ def about(request):
   return render(request, 'about.html')
 
 def characters_index(request):
+  characters = Character.objects.all()
   return render(request, 'characters/index.html', {
     'characters': characters
   })
