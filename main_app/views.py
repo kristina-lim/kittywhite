a17dd1b19df410c3f8f23a85a61fada44cd521af
kristina-lim <kristinahlim@gmail.com>
2023-01-12 15:48:24 -0800

    Code signup view function

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Character, Hobby, Photo
 from .forms import FeedingForm
 
@@ -96,3 +98,17 @@ def add_photo(request, character_id):
       print('An error occurred uploading file to S3')
       print(e)
   return redirect('detail', character_id=character_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
